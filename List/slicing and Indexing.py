8af335e8dbb2c8481030ced405ee1b8337b3212e
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-06 15:09:47 +0600

    practice slicing and indexing

---
+++ List/slicing and Indexing.py
@@ -0,0 +1,11 @@
+#slicing
+numbers = [0,1,2,3,4,5,6,7,8,9];
+
+#numbers[start:end:step]
+print(numbers[2:6])
+print(numbers[:6])
+print(numbers[:])
+print(numbers[2:])
+print(numbers[2:6:2])
+
+print(numbers[-1::-1])
