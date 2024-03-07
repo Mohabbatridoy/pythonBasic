43ff0e4a685ac65aa4181f4cfdabd822bdaf959f
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-07 19:47:48 +0600

    practice practice string

---
+++ String/string variable.py
@@ -0,0 +1,44 @@
+# paragraph = '''this is first line
+#
+# this is second line
+# this is third line
+# this is jemon lekhci temoni
+# '''
+# print(paragraph)
+
+# ###Uppercase:
+# userName = "sifat_hassan"
+# #upper
+# print(userName.upper())
+# #lower()
+# name = "MOHABBAT"
+# lowerName=name.lower()
+# print(lowerName)
+
+# ###Replace(oldvalue,nevalue,count):
+# welcomeText = "welcome! to ! javascript!!"
+# modifiedWelcomeText = welcomeText.replace("!","#",2)
+# print(modifiedWelcomeText)
+
+###split():
+# fruits = "apple,mango,tata,bata"
+# fruitsList = fruits.split(",")
+# print(fruitsList)
+
+# name = "MOhabbat Hossain"
+# print(name.split(" "))
+#
+# ###slice()
+# firstName = name[:8]
+# print(firstName)
+# lastName = name[9:]
+# print(lastName)
+
+###concat: the meaning is add two string:
+first_name = "Mohabbat"
+last_name = "Hridoy"
+fullName = first_name +" "+ last_name
+print(fullName)
+wel = "Welcome"
+cmpt = wel+ " "+fullName
+print(cmpt)
