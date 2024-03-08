8ff0688c553c504af908f6380200861eea839cda
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-08 12:10:20 +0600

    practice string formating and Escape character

---
+++ String/Escape character.py
@@ -0,0 +1,8 @@
+String = "Mohabbat is try to be \"a good Muslim\""
+print(String)
+
+newLine = "I am ceating a \nnew line"
+print(newLine)
+
+tap = "I am giving a tap in \t here"
+print(tap)
---
+++ String/String_Formating.py
@@ -0,0 +1,8 @@
+firstName = "Mohabbat"
+LastName = "Hridoy"
+id = 4508
+fullName = "Name {firstName} {LastName} and userID {id}".format(firstName=firstName,
+                     LastName=LastName,
+                     id=id)
+
+print(fullName)
