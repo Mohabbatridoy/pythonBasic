c71998b71a1395ae8c7294f779d3e2d18f1e6310
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-08 18:07:34 +0600

    practice Dictinonary

--- Data sturcture/Dictionary.py
+++ Data sturcture/Dictionary.py
@@ -13,25 +13,93 @@
 # print(student)
 
 #access item from dictionary:
-student = {
-    "name": "Mohabbat Hossain",
-    "skills": ["python","c","javascript","java"],
-    "id": "213-15-4508"
+# student = {
+#     "name": "Mohabbat Hossain",
+#     "skills": ["python","c","javascript","java"],
+#     "id": "213-15-4508"
+# }
+#
+# # print(student["name"])
+# # print(student["skills"])
+# # print(student.get("ida","not found! please check the key."))
+#
+# ##=> how to change dictionary item:
+# student["name"] = "Mohabbat Hossain Ridoy"
+# print(student)
+#
+# #=> change multiple item using update() method:
+# student.update(
+#     {
+#         "name": "Rezwan coder",
+#         "id": "213-15-4559"
+#     }
+# )
+# print(student)
+
+
+# ##Add item in dictionary:
+# student = {
+#     "firstName": "Mohabbat",
+#     "lastName": "Hridoy",
+#     "id": "213-15-4508"
+# }
+#
+# student["middleName"] = "Hossain"
+# student.update(
+#     {
+#         "University": "Daffodil Internationa University"
+#     }
+# )
+# print(student)
+
+
+#=> Remove Item:
+# std = {'firstName': 'Mohabbat',
+#        'lastName': 'Hridoy',
+#        'id': '213-15-4508',
+#        'middleName': 'Hossain',
+#        'University': 'Daffodil Internationa University'
+# }
+
+# del std['middleName']
+# print(std)
+# print(std.pop("lastName"))
+# print(std)
+
+# ##=> iterate dictionary by loop:
+# #keys
+# # print(std.keys())
+# print(std.items())
+# # print(std.values())
+#
+# for i in std.keys():
+#     print(i, std[i])
+# print("**************************")
+# for key, item in std.items():
+#     print(key, item)
+
+####=> nested dictionary:
+document = {
+    1: {
+        "name": "Mhr",
+        "id": 4508
+    },
+    2: {
+        "name": "hridoy",
+        "id": 4208
+    }
 }
 
-# print(student["name"])
-# print(student["skills"])
-# print(student.get("ida","not found! please check the key."))
-
-##=> how to change dictionary item:
-student["name"] = "Mohabbat Hossain Ridoy"
-print(student)
-
-#=> change multiple item using update() method:
-student.update(
+document[1]["name"] = "Mohabbat"
+document[1]["cg"] = "3.60"
+document.update(
     {
-        "name": "Rezwan coder",
-        "id": "213-15-4559"
+        3: {
+            "name": "Hriody",
+            "id": "213-154508"
+        }
     }
 )
-print(student)
+print(document)
+
+print(document[1]["id"])
