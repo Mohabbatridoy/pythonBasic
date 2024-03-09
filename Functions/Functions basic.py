3a67aedd14feed62ceabf1d8b4066c79d38c53e9
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-09 19:55:13 +0600

    practice function

---
+++ Functions/Functions basic.py
@@ -0,0 +1,13 @@
+# def greatings():
+#     print("hi mi kai khai dai tai cbai nai cai tai asdfj")
+#
+# std1 = "mhr"
+# greatings()
+#
+# std2 = "m2"
+# greatings()
+#
+# std3 = "hri"
+# greatings()
+
+###=> keyword argument:
