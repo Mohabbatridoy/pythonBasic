1c80d216e1474cdddddf0dcb31d4ad126997aa4d
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-14 12:53:18 +0600

    practice deleting file

--- Python file/file.py
+++ Python file/file.py
@@ -1,27 +1,37 @@
-import os
-path = '../Functions'
-# # os.listdir(path)
-# all_files = os.listdir(path)
+# import os
+# path = '../Functions'
+# # # os.listdir(path)
+# # all_files = os.listdir(path)
+# #
+# # for files in all_files :
+# #     if os.path.isfile(os.path.join(path, files)):
+# #         print("{} is a file".format(files))
 #
-# for files in all_files :
-#     if os.path.isfile(os.path.join(path, files)):
-#         print("{} is a file".format(files))
+#
+# ##=>  os.scandir() method:
+#
+# all_files = os.scandir(path)
+# for file in all_files:
+#     if file.is_file():
+#         print("{} is a file".format(file.name))
+#
+# ## using pathlib module:
+#
+# import pathlib
+#
+# path_obj = pathlib.Path(path)
+# for file in path_obj.iterdir():
+#     if file.is_file():
+#         print(file.name)
 
+##=> check if a file exists or not:
+path = 'testFile'
 
-##=>  os.scandir() method:
+#using os.path:
+import os
+print(os.path.isfile(path))
 
-all_files = os.scandir(path)
-for file in all_files:
-    if file.is_file():
-        print("{} is a file".format(file.name))
-
-## using pathlib module:
-
+#using pathlib.Path:
 import pathlib
-
-path_obj = pathlib.Path(path)
-for file in path_obj.iterdir():
-    if file.is_file():
-        print(file.name)
-
-
+file = pathlib.Path(path)
+print(file.is_file())
--- Python file/read,write,append.py
+++ Python file/read,write,append.py
@@ -41,25 +41,34 @@
 #     file.write("this is for test purpose")
 
 
-##problem :
-user_data = [
-    {
-        'fileName': 'user1.tet',
-        'context' : "Hello This is from user 1"
-    },
-    {
-        'file_name' : 'user2',
-        'context' : "Hello this is from user 2"
-    },
-    {
-        'file_name': 'user3',
-        'context': "Hello this is from user 3"
-    }
-]
+# ##problem :
+# user_data = [
+#     {
+#         'fileName': 'user1.tet',
+#         'context' : "Hello This is from user 1"
+#     },
+#     {
+#         'file_name' : 'user2',
+#         'context' : "Hello this is from user 2"
+#     },
+#     {
+#         'file_name': 'user3',
+#         'context': "Hello this is from user 3"
+#     }
+# ]
+#
+# with open('user1', 'w') as file:
+#     file.write("Hello This is from user 1")
+# with open('user2', 'w') as file:
+#     file.write("Hello This is from user 2")
+# with open('user3', 'w') as file:
+#     file.write("Hello This is from user 3")
 
-with open('user1', 'w') as file:
-    file.write("Hello This is from user 1")
-with open('user2', 'w') as file:
-    file.write("Hello This is from user 2")
-with open('user3', 'w') as file:
-    file.write("Hello This is from user 3")
+##=> deleting a file:
+import os
+path = 'user1'
+if os.path.isfile(path) :
+    os.remove(path)
+    print("successfully removed")
+else:
+    print("the file is not exits")
