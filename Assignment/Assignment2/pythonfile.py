585c9fd98db77ab604f5197972df66ea672d4a97
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-17 23:19:30 +0600

    Assignment 2

---
+++ Assignment/Assignment2/pythonfile.py
@@ -0,0 +1,17 @@
+with open('WordFile','r') as file:
+    a = file.read()
+
+fileList = a.split("\n")
+# print(fileList)
+
+search_words = ['Python', 'C', 'OOP', "Hello", "Java"]
+
+for search_words in search_words:
+    # print(search_words)
+    count = 0
+    for file in fileList:
+        if search_words == file:
+            count += 1
+        else:
+            continue
+    print(f"{search_words}-> {count}")
