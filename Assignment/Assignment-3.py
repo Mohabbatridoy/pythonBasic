af3117792d295c6d7509349ea814c58c5fb979a2
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-25 22:10:50 +0600

    Assignment -3

--- Assignment/Assignment-3.py
+++ Assignment/Assignment-3.py
@@ -2,35 +2,47 @@ import random
 low = 1
 high = 50
 
-correct_answer = random.randint(low,high)
-user_number = 0
-print(correct_answer)
+# correct_answer = random.randint(low,high)
+# user_number = 0
+# print(correct_answer)
+
+def restartOrExit():
+    print("1.Play again \n2.Exit")
+    n = int(input("Enter 1 or 2:\n"))
+    if n == 1:
+        numberGuser()
+    elif n == 2:
+        print("Game Exit")
+        return
+    else:
+        print("Invalid! Enter 1 to play again or 2 to exit game")
+    restartOrExit()
 
 def numberGuser():
+
+    correct_answer = random.randint(low, high)
+    print(correct_answer)
+    user_number = 0
+
     for i in range(0, 5):
         print(f"You have {5 - i} chance left")
         user_number = int(input(f"Guess a number between 1 and 50: "))
+
         if user_number == correct_answer:
             print("YOU WIN")
+            print("\ngame over\n")
+            restartOrExit()
             break;
         elif user_number < correct_answer:
             print("Correct answer is greater!")
         elif user_number > correct_answer:
             print("Correct answer is smaller!")
-        else:
-            print("You have entered invalid number")
         print("*****************************")
+
 
     if user_number != correct_answer:
         print("YOU LOSE")
+        print("\ngame over\n")
+        restartOrExit()
 
 numberGuser()
-print("\ngame over\n")
-print("1.Play again \n2.Exit")
-n = int(input("Enter 1 or 2:\n"))
-if n == 1:
-    numberGuser()
-elif n == 2:
-    print("Game Exit")
-else:
-    print("Enter 1 or 2")
