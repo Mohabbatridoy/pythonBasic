d080f90ad360221a6de91930e8d07fb19bedc1d9
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-03-26 15:18:20 +0600

    practice dictionary

--- PythonForDjango/dictionary.py
+++ PythonForDjango/dictionary.py
@@ -8,3 +8,27 @@ myDict['name'] = 'mohabbat'
 myDict['id'] = '4508'
 # del myDict['name']
 print(myDict)
+
+#another aproch to declare dictionary usnig dict():
+a = dict(key1 = "Mohabbat" , mh = "Bohubrihi", key3 = "22")
+
+b = {
+    'key1': "mhr"
+}
+c = dict(zip(['key1','key2'],['Mohabbat','Hridoy']))
+d = dict(
+    [
+        ('key','Mohabbat'),
+        ('key1','Ridoy')
+    ]
+)
+
+e = dict({'key1': "mhr", 'key2':'ridoy'},key3 = "hfdfg",key4 = "sfdg",key5 = "dfg")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(e.pop('key2'))
+keyOfE = list(e)
+print(keyOfE)
