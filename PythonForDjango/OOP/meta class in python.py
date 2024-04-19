2668bb7a4be1b682aa65815c97e05ae8e6da455f
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-04-19 19:30:06 +0600

    practice oop

---
+++ PythonForDjango/OOP/meta class in python.py
@@ -0,0 +1,8 @@
+#meta class is a class of all class:
+#everything in python is object. a class of oop is also a object of 'type' class
+#here type is a meta class.
+class A :
+    pass
+obj = A()
+print(type(obj))
+print(type(A))
