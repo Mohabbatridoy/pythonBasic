ae46a6768267d0046fe8782525c2ea515ab8892b
Md. Mohabbat Hossain Ridoy <110348466+Mohabbatridoy@users.noreply.github.com>
2024-04-19 20:06:28 +0600

    practice python module

---
+++ PythonForDjango/Python Module/import statement.py
@@ -0,0 +1,16 @@
+# import math
+# print(math.ceil(5.6))
+
+# import oopForImport
+# print(oopForImport.add(23,32))
+# a = oopForImport.pi
+# b = 2+ a
+# print(b)
+#
+# obj = oopForImport.A()
+
+## from statement:
+from oopForImport import add
+print(add(23,32))
+
+from oopForImport import * #=> to import everything:
